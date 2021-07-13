b597703400bf0fe4104726e3b7ffac5f92012f40
matiasgrana <maicolmatiasg@gmail.com>
2021-07-13 14:20:58 -0300

    Update event_handler_nsclient_command.sh

--- files/plugins/event_handler_nsclient_command.sh
+++ files/plugins/event_handler_nsclient_command.sh
@@ -5,14 +5,14 @@ state=$1 # eg. "OK","CRITICAL,"WARNING","UNKNOWN"
 statetype=$2 # eg. "SOFT","HARD"
 host=$3 # hostaddress of where to execute nrpe command
 command=$4 # nrpe command to execute
-logfile=/var/log/eventhandler.log # logfile to store executions by this eventhandler
+logfile=/tmp/eventhandler.log  # logfile to store executions by this eventhandler
 
 # Date format: "2016-03-29 13:10 CEST"
 date=`date +"%Y-%m-%d %H:%M %Z"`
 case "$1" in (CRITICAL)
  if [ "$statetype" = "HARD" ] ; then
  /bin/echo -en "$date | ${0##*/} Got state: <$state> and statetype: <$statetype> with command <$command> and argument <$cmdarg> for execution on host <$host>\n" >> $logfile
- /usr/lib/nagios/plugins/check_nrpe -H $host -c $command >> $logfile
+ /usr/lib/nagios/plugins/check_nrpe -H $host -p 5666 -t 30 -c $command >> $logfile
  fi
 esac
 exit 0
